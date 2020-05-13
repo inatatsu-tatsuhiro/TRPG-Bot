2029ea8148e138d791ea222614cc91ace22d631f
inatatsu <tatsuhiro.9699@gmail.com>
2020-05-13 22:27:50 +0900

    removed comment

--- main.py
+++ main.py
@@ -2,28 +2,23 @@ from discord.ext import commands # Bot Commands Frameworkをインポート
 import os
 import traceback # エラー表示のためにインポート
 
-# 読み込むコグの名前を格納しておく。
+
 INITIAL_EXTENSIONS = [
     'cogs.cthulhucog'
 ]
 
-# クラスの定義。ClientのサブクラスであるBotクラスを継承。
 class MyBot(commands.Bot):
 
-    # MyBotのコンストラクタ。
+
     def __init__(self, command_prefix):
-        # スーパークラスのコンストラクタに値を渡して実行。
         super().__init__(command_prefix)
 
-        # INITIAL_COGSに格納されている名前から、コグを読み込む。
-        # エラーが発生した場合は、エラー内容を表示。
         for cog in INITIAL_EXTENSIONS:
             try:
                 self.load_extension(cog)
             except Exception:
                 traceback.print_exc()
 
-    # Botの準備完了時に呼び出されるイベント
     async def on_ready(self):
         print('-----')
         print(self.user.name)
@@ -31,7 +26,6 @@ class MyBot(commands.Bot):
         print('-----')
 
 
-# MyBotのインスタンス化及び起動処理。
 if __name__ == '__main__':
     bot = MyBot(command_prefix='!')
     bot.run(os.environ["TOKEN"]) # botトークンを環境変 TOKEN を使う
