b4387d8fe9f745fba810cb166d7238c2b3f90b1f
inatatsu <tatsuhiro.9699@gmail.com>
2020-05-17 12:18:23 +0900

    fix

---
+++ cogs/gamestatus.py
@@ -0,0 +1,63 @@
+from discord.ext import commands
+from cogs.status import Status
+
+
+class GameStatus(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
+
+    @commands.command()
+    async def create(self, ctx):
+        """セッションを立てる"""
+
+        if self.bot.game.status == Status.PLAYING:
+            await ctx.send('セッション中です')
+            return
+        if self.bot.game.status == Status.WAITING:
+            await ctx.send('セッション準備中')
+            return
+
+        self.bot.game.status = Status.WAITING
+        self.bot.game.channel = ctx.channel
+        await ctx.send('セッションの準備を開始します')
+
+
+
+
+    @commands.command()
+    async def start(self, ctx):
+        """セッション開始"""
+        if self.bot.game.status == Status.NOTHING:
+            await ctx.send('セッションが立ってません')
+            return
+        if self.bot.game.status == Status.PLAYING:
+            await ctx.send('セッション中です')
+            return
+
+        self.bot.game.status = Status.PLAYING
+        await ctx.send('セッション開始しました')
+
+    
+    @commands.command()
+    async def close(self, ctx):
+        """セッション終了"""
+        if self.bot.game.status == Status.NOTHING:
+            await ctx.send('セッションが立ってません')
+            return
+        if self.bot.game.status == Status.WAITING:
+            self.bot.game.status = Status.NOTHING
+            await ctx.send('セッションをキャンセルします')
+            return
+        self.bot.game.status = Status.NOTHING
+        await ctx.send('セッションを終了します')
+
+
+
+def setup(bot):
+    bot.add_cog(GameStatus(bot))
